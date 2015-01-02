009f358415c314a12dc95198a5b294e70ec73b9b
kusut <tinokusut@gmail.com>
2015-01-02 21:18:18 +0700

    refactor code, add comments

--- src/Site.hs
+++ src/Site.hs
@@ -5,48 +5,64 @@ import           Hakyll
 import           Text.Highlighting.Kate (pygments, styleToCss)
 -------------------------------------------------------------------------------
 
+-- kompres berkas2 css
+cssCompressor :: Rules ()
+cssCompressor = match "static/css/*" $ do
+                  route   idRoute
+                  compile compressCssCompiler
+
+
+-- generate css untuk sintaks highlight
+cssHighlight :: Rules ()
+cssHighlight = create ["static/css/style.css"] $ do
+                 route idRoute
+                 compile $ makeItem (compressCss . styleToCss $ pygments)
+
+
+-- lecture index, compiled to /lectures.html
+lectureIndex :: Rules ()
+lectureIndex = match "lectures/lectures.md" $ do
+                 route $ gsubRoute "lectures/" (const "") `composeRoutes` setExtension "html"
+                 compile $ pandocCompiler
+                   >>= loadAndApplyTemplate "templates/body.html" defaultContext
+                   >>= loadAndApplyTemplate "templates/base.html" defaultContext
+                   >>= relativizeUrls
+
+
+-- lecture materials, html version
+lectureHtml :: Rules ()
+lectureHtml = match "lectures/*" $ do
+                route $ setExtension "html"
+                compile $ pandocCompiler
+                  >>= loadAndApplyTemplate "templates/body.html" defaultContext
+                  >>= loadAndApplyTemplate "templates/base.html" defaultContext
+                  >>= relativizeUrls
+
+
+-- lecture materials, raw lhs files
+lectureRaw :: Rules ()
+lectureRaw = match "lectures/*" $ version "raw" $ do
+               route   idRoute
+               compile getResourceBody
+
+
+templates :: Rules ()
+templates = match "templates/*" $ compile templateCompiler
+
+siteIndex :: Rules ()
+siteIndex = match "index.html" $ do
+              route idRoute
+              compile $ do
+                getResourceBody
+                  >>= loadAndApplyTemplate "templates/base.html" defaultContext
+                  >>= relativizeUrls
+
 main :: IO ()
 main = hakyll $ do
-
-    match "static/css/*" $ do
-        route   idRoute
-        compile compressCssCompiler
-
-    create ["static/css/style.css"] $ do
-        route idRoute
-        compile $ makeItem (compressCss . styleToCss $ pygments)
-
-    match "static/images/**" $ do
-        route   idRoute
-        compile copyFileCompiler
-
-    -- lecture index, compiled to /lectures.html
-    match "lectures/lectures.md" $ do
-        route $ gsubRoute "lectures/" (const "") `composeRoutes` setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/body.html" defaultContext
-            >>= loadAndApplyTemplate "templates/base.html" defaultContext
-            >>= relativizeUrls
-
-    -- lecture materials, html version
-    match "lectures/*" $ do
-        route $ setExtension "html"
-        compile $ pandocCompiler
-            >>= loadAndApplyTemplate "templates/body.html" defaultContext
-            >>= loadAndApplyTemplate "templates/base.html" defaultContext
-            >>= relativizeUrls
-
-    -- lecture materials, raw lhs files
-    match "lectures/*" $ version "raw" $ do
-        route   idRoute
-        compile getResourceBody
-
-    match "index.html" $ do
-         route idRoute
-         compile $ do
-             getResourceBody
-                 >>= loadAndApplyTemplate "templates/base.html" defaultContext
-                 >>= relativizeUrls
-
-    match "templates/*" $ compile templateCompiler
-
+         cssCompressor
+         cssHighlight
+         lectureIndex
+         lectureHtml
+         lectureRaw
+         templates
+         siteIndex
